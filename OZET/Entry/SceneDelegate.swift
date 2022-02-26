7f5c0fe140829c115b3e0e1cfeacb783fe85db13
이의석 <euisuk.lee@kurlycorp.com>
2022-02-26 19:49:04 +0900

    fix: default 계정 제거

--- OZET/Entry/SceneDelegate.swift
+++ OZET/Entry/SceneDelegate.swift
@@ -15,9 +15,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
   func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
     guard let windowScene = (scene as? UIWindowScene) else { return }
     
-    UserDefaults.standard.set("eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjo5LCJ1c2VybmFtZSI6Im96ZXRfZDE2MDY2ZjA5YjU5NDI3NmJiN2Q5NjI4ZTVlYTE1NjQiLCJleHAiOjE2NDYyMDEzNDl9.CQfCd1VtNL051mvnHq2-OzqHe5nbpte_6bWXqfi7aJ0", forKey: "token")
-    UserDefaults.standard.synchronize()
-
     let window = UIWindow(windowScene: windowScene)
     let reactor = MainReactor(
       announcementService: AnnouncementServiceImpl(
